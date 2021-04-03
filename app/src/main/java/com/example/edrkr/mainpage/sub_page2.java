c0de1dcae806c1aa3d5fd1419c2aa2bede5772d0
Hyunji99Choi <tyg06229@gmail.com>
2021-04-03 20:41:27 +0900

    0403 비회원이신가요? chart 초기 형태 구성

--- app/src/main/java/com/example/edrkr/mainpage/sub_page2.java
+++ app/src/main/java/com/example/edrkr/mainpage/sub_page2.java
@@ -17,28 +17,41 @@ import com.example.edrkr.R;
 import com.example.edrkr.UserIdent;
 import com.example.edrkr.h_network.AutoRetryCallback;
 import com.example.edrkr.h_network.ResponseGraphJson;
-import com.example.edrkr.h_network.ResponseWeatherJson;
 import com.example.edrkr.h_network.RetrofitClient;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.ValueFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
+
+import org.eazegraph.lib.charts.BarChart;
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.BarModel;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.listener.ColumnChartOnValueSelectListener;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.Chart;
+import lecho.lib.hellocharts.view.ColumnChartView;
+import lecho.lib.hellocharts.view.LineChartView;
 import retrofit2.Call;
 import retrofit2.Response;
 
 public class sub_page2 extends Fragment {
 
-    private LineChart chart;
+    private LineChartView chart;
+
 
     @Nullable
     @Override
@@ -46,16 +59,40 @@ public class sub_page2 extends Fragment {
 
         View view=inflater.inflate(R.layout.mainpage_sub_page2,container,false);
 
-        chart=(LineChart) view.findViewById(R.id.LineChart);
-        initChartSetting();
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+
+        }
+        //chart = view.findViewById(R.id.chart);
+
+        //initChartSetting();
+
+
+        //그래프 값 통신
+        getGreahData();
+
         return view;
 
     }
     private void initChartSetting(){
 
+        List<PointValue> values = new ArrayList<PointValue>();
+        values.add(new PointValue(0, 2));
+        values.add(new PointValue(1, 4));
+        values.add(new PointValue(2, 3));
+        values.add(new PointValue(3, 4));
 
+        //In most cased you can call data model methods in builder-pattern-like manner.
+        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
 
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
 
+        chart.setLineChartData(data);
+
+        /*
         final ArrayList<String> labels = new ArrayList<String>();
         labels.add("오전 6시");
         labels.add("오전 8시");
@@ -64,7 +101,6 @@ public class sub_page2 extends Fragment {
         labels.add("오후 2시");
         labels.add("오후 4시");
         labels.add("오후 6시");
-
 
         ArrayList<Entry> entries = new ArrayList<Entry>();
         entries.add(new Entry(0, 4f));
@@ -93,7 +129,6 @@ public class sub_page2 extends Fragment {
         YAxis yAxis = chart.getAxisLeft();
 
 
-
         //데이터셋 설정
         LineDataSet lineDataSet = new LineDataSet(entries, "습도");
         //lineDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
@@ -106,14 +141,16 @@ public class sub_page2 extends Fragment {
 
         LineData lineData = new LineData(lineDataSet);
 
-
         chart.setData(lineData);
         chart.invalidate();
 
         //chart.animateY(5000);
+         */
 
-        //그래프 값 통신
-        getGreahData();
+
+
+
+
     }
 
     private void getGreahData(){
@@ -149,5 +186,155 @@ public class sub_page2 extends Fragment {
             }
         });
     }
+
+
+    //복붙
+    public static class PlaceholderFragment extends Fragment {
+        public final static String[] months = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
+                "Sep", "Oct", "Nov", "Dec",};
+
+        public final static String[] days = new String[]{"Mon", "Tue", "Wen", "Thu", "Fri", "Sat", "Sun",};
+
+        private LineChartView chartTop;
+        private ColumnChartView chartBottom;
+
+        private LineChartData lineData;
+        private ColumnChartData columnData;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.chart_previewlinechart, container, false);
+
+            // *** TOP LINE CHART ***
+            chartTop = (LineChartView) rootView.findViewById(R.id.chart_top);
+
+            // Generate and set data for line chart
+            generateInitialLineData();
+
+            // *** BOTTOM COLUMN CHART ***
+
+            chartBottom = (ColumnChartView) rootView.findViewById(R.id.chart_bottom);
+
+            generateColumnData();
+
+            return rootView;
+        }
+
+    private void generateColumnData() {
+
+        int numSubcolumns = 1;
+        int numColumns = months.length;
+
+        List<AxisValue> axisValues = new ArrayList<AxisValue>();
+        List<Column> columns = new ArrayList<Column>();
+        List<SubcolumnValue> values;
+        for (int i = 0; i < numColumns; ++i) {
+
+            values = new ArrayList<SubcolumnValue>();
+            for (int j = 0; j < numSubcolumns; ++j) {
+                values.add(new SubcolumnValue((float) Math.random() * 50f + 5, ChartUtils.pickColor()));
+            }
+
+            axisValues.add(new AxisValue(i).setLabel(months[i]));
+
+            columns.add(new Column(values).setHasLabelsOnlyForSelected(true));
+        }
+
+        columnData = new ColumnChartData(columns);
+
+        columnData.setAxisXBottom(new Axis(axisValues).setHasLines(true));
+        columnData.setAxisYLeft(new Axis().setHasLines(true).setMaxLabelChars(2));
+
+        chartBottom.setColumnChartData(columnData);
+
+        // Set value touch listener that will trigger changes for chartTop.
+        chartBottom.setOnValueTouchListener(new ValueTouchListener());
+
+        // Set selection mode to keep selected month column highlighted.
+        chartBottom.setValueSelectionEnabled(true);
+
+        chartBottom.setZoomType(ZoomType.HORIZONTAL);
+
+        // chartBottom.setOnClickListener(new View.OnClickListener() {
+        //
+        // @Override
+        // public void onClick(View v) {
+        // SelectedValue sv = chartBottom.getSelectedValue();
+        // if (!sv.isSet()) {
+        // generateInitialLineData();
+        // }
+        //
+        // }
+        // });
+
+    }
+
+    private void generateInitialLineData() {
+        int numValues = 7;
+
+        List<AxisValue> axisValues = new ArrayList<AxisValue>();
+        List<PointValue> values = new ArrayList<PointValue>();
+        for (int i = 0; i < numValues; ++i) {
+            values.add(new PointValue(i, 0));
+            axisValues.add(new AxisValue(i).setLabel(days[i]));
+        }
+
+        Line line = new Line(values);
+        line.setColor(ChartUtils.COLOR_GREEN).setCubic(true);
+
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        lineData = new LineChartData(lines);
+        lineData.setAxisXBottom(new Axis(axisValues).setHasLines(true));
+        lineData.setAxisYLeft(new Axis().setHasLines(true).setMaxLabelChars(3));
+
+        chartTop.setLineChartData(lineData);
+
+        // For build-up animation you have to disable viewport recalculation.
+        chartTop.setViewportCalculationEnabled(false);
+
+        // And set initial max viewport and current viewport- remember to set viewports after data.
+        Viewport v = new Viewport(0, 110, 6, 0);
+        chartTop.setMaximumViewport(v);
+        chartTop.setCurrentViewport(v);
+
+        chartTop.setZoomType(ZoomType.HORIZONTAL);
+    }
+
+    private void generateLineData(int color, float range) {
+        // Cancel last animation if not finished.
+        chartTop.cancelDataAnimation();
+
+        // Modify data targets
+        Line line = lineData.getLines().get(0);// For this example there is always only one line.
+        line.setColor(color);
+        for (PointValue value : line.getValues()) {
+            // Change target only for Y value.
+            value.setTarget(value.getX(), (float) Math.random() * range);
+        }
+
+        // Start new data animation with 300ms duration;
+        chartTop.startDataAnimation(300);
+    }
+
+    private class ValueTouchListener implements ColumnChartOnValueSelectListener {
+
+        @Override
+        public void onValueSelected(int columnIndex, int subcolumnIndex, SubcolumnValue value) {
+            generateLineData(value.getColor(), 100);
+        }
+
+        @Override
+        public void onValueDeselected() {
+
+            generateLineData(ChartUtils.COLOR_GREEN, 0);
+
+        }
+    }
+}
 
 }
