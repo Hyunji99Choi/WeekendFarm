8def475df6f886c4bb006403aea48c45350bce2b
Hyunji99Choi <tyg06229@gmail.com>
2021-04-27 12:24:04 +0900

    04.27 material toggleGroup success, 버튼 연결

--- app/src/main/java/com/example/edrkr/mainpage/sub_page3.java
+++ app/src/main/java/com/example/edrkr/mainpage/sub_page3.java
@@ -14,16 +14,21 @@ import androidx.fragment.app.Fragment;
 import com.example.edrkr.R;
 import com.example.edrkr.dailyMemo.dailyMemo_recyclerview;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
 
 public class sub_page3 extends Fragment implements View.OnClickListener {
 
     dailyMemo_recyclerview calendar;
+    MaterialButtonToggleGroup toggleGroup;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view=inflater.inflate(R.layout.mainpage_sub_page3,container,false);
 
+        toggleGroup = view.findViewById(R.id.toggleGroup);
         MaterialButton year = view.findViewById(R.id.year);
         MaterialButton month = view.findViewById(R.id.month);
         MaterialButton week = view.findViewById(R.id.week);
@@ -36,13 +41,17 @@ public class sub_page3 extends Fragment implements View.OnClickListener {
 
         return view;
 
-
-
     }
 
 
     @Override
     public void onClick(View v) {
+        //전부 선택 해제
+        if(toggleGroup.getCheckedButtonId()==View.NO_ID){
+            calendar.changeMode(0);
+            return;
+        }
+
         switch (v.getId()){
             case R.id.year:
                 calendar.changeMode(1);
