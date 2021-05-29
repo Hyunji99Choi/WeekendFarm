fbcaf079b946cf7afa440eef5b1dd8eb771fdb82
moonbow-ii <un9011@gmail.com>
2021-05-29 21:56:09 +0900

    shingshingInfo edit+add some data

--- app/src/main/java/com/example/edrkr/shingshing_info.java
+++ app/src/main/java/com/example/edrkr/shingshing_info.java
@@ -85,6 +85,8 @@ public class shingshing_info extends YouTubeBaseActivity {
         TextView insectName;
         ImageView insectImg;
         TextView insectInfo;
+        TextView insectDamegeInfo;
+        TextView insectPreventInfo;
         TextView insectAImgName;
         ImageView insectAImg;
         TextView cookCropName;
@@ -117,6 +119,8 @@ public class shingshing_info extends YouTubeBaseActivity {
         insectName = (TextView)findViewById(R.id.insName);
         insectImg = (ImageView)findViewById(R.id.insImg);
         insectInfo = (TextView)findViewById(R.id.insInfo);
+        insectDamegeInfo = (TextView)findViewById(R.id.insDamageInfo);
+        insectPreventInfo = (TextView)findViewById(R.id.insPreventInfo);
         insectAImgName = (TextView)findViewById(R.id.insAIMGName);
         insectAImg = (ImageView)findViewById(R.id.insAIMG);
         cookCropName = (TextView)findViewById(R.id.cookName);
@@ -160,7 +164,7 @@ public class shingshing_info extends YouTubeBaseActivity {
                     return;
                 }
                 //Log.d("싱싱정보통 통신 성공적",response.body().toString());
-                String value=response.body().replace("[\"","\"").replace("\"]","");
+                String value=response.body().replace("[\"","\"").replace("\"]","").replace("<br/>","\n\n").replace("\\r\\n","\n\n");
                 String[] splitvalue=value.split(",\"");
 
 
@@ -181,17 +185,17 @@ public class shingshing_info extends YouTubeBaseActivity {
 
                 //이부분 아직 null이 온적이 없어서 null이 오면 어떻게 되는지 잘 모름
                 if(splitvalue[6]==null)
-                    BugExplain[0]="";//병충해 설명1
+                    BugExplain[0]="";//병충해 설명1 = 생태정보
                 else
                     BugExplain[0]= splitvalue[6];//병충해 설명1
 
                 if(splitvalue[7]==null)
-                    BugExplain[1]="";//병충해 설명2
+                    BugExplain[1]="";//병충해 설명2 - 피해정보
                 else
                     BugExplain[1]= splitvalue[7];//병충해 설명2
 
                 if(splitvalue[8]==null)
-                    BugExplain[2]="";//병충해 설명2
+                    BugExplain[2]="";//병충해 설명3 - 방제방법
                 else
                     BugExplain[2]= splitvalue[8];//병충해 설명2
 
@@ -205,28 +209,6 @@ public class shingshing_info extends YouTubeBaseActivity {
 
                 YoutubeSet(youTubePid);
 
-                //-UI
-
-/*                Thread th = new Thread(){
-                    @Override
-                    public void run() {
-                        try{
-                            URL url = new URL("http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer.mo?uploadSpec=npms&uploadSubDirectory=/photo/kncr/&imageFileName=FC050501[20110114150000000].jpg");
-                            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-                            conn.setDoInput(true);
-                            conn.connect();
-
-                            InputStream is = conn.getInputStream();
-                            img1 = BitmapFactory.decodeStream(is);
-                        }catch (MalformedURLException e){
-                            e.printStackTrace();
-                        }catch (IOException e){
-                            e.printStackTrace();
-                        }
-                    }
-                };
-                */
-
                 cropImg.setImageBitmap(getBitmap(firstImg));
                 stCropName.setText(firstName);
                 ndCropName.setText(secondName);
@@ -237,6 +219,8 @@ public class shingshing_info extends YouTubeBaseActivity {
                 insectName.setText(BugName);
                 insectImg.setImageBitmap(getBitmap(BugImg));
                 insectInfo.setText(BugExplain[0]);
+                insectDamegeInfo.setText(BugExplain[1]);
+                insectPreventInfo.setText(BugExplain[2]);
                 insectAImgName.setText(BugHardName);
                 insectAImg.setImageBitmap(getBitmap(BugHardImg));
                 cookCropName.setText(firstName+" 요리 레시피");
